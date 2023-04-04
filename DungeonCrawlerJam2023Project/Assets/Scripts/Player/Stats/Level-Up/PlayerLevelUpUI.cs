718ccf908449c2b6b9a344226bfc776415ddb0b6
Reece Tarrant <rtarrant70998@gmail.com>
2023-04-05 00:26:03 +0100

    Added Level-Up UI

---
+++ DungeonCrawlerJam2023Project/Assets/Scripts/Player/Stats/Level-Up/PlayerLevelUpUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerLevelUpUI : MonoBehaviour {
+    public TextMeshProUGUI levelText;
+    public Slider xpMeter;
+
+    void Update() {
+        levelText.text = "Level " + GameObject.Find("Player(Clone)").GetComponent<PlayerLevelScript>().currentLevel.levelNumber;
+        if(GameObject.Find("Player(Clone)").GetComponent<PlayerLevelScript>().currentLevel.levelNumber < GameObject.Find("Player(Clone)").GetComponent<PlayerLevelScript>().levels.Length) {
+             xpMeter.maxValue = GameObject.Find("Player(Clone)").GetComponent<PlayerLevelScript>().levels[GameObject.Find("Player(Clone)").GetComponent<PlayerLevelScript>().currentLevel.levelNumber].xpToReachLevel;
+        }
+        xpMeter.value = GameObject.Find("Player(Clone)").GetComponent<PlayerLevelScript>().currXP;
+    }
+}
